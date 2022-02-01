78d7cdaa872749d97f75b7fb27c68e144e29587d
Fabrizio-Turdo <fabrizioturdo1999@gmail.com>
2022-01-31 21:33:31 -0300

    Entrega menu E-commerce

---
+++ src/components/Navbar/NavBar.js
@@ -0,0 +1,17 @@
+import './NavBar.css'
+
+const Navbar = () => {
+    return (
+        
+         <nav className="Navbar">
+             <h1>FastBurger</h1>
+             <ul className="Ul">
+                 <li className="Li"><a href="#">Combos</a></li>
+                 <li className="Li"><a href="#">Bebidas</a></li>
+             </ul>
+        </nav>
+
+
+    );
+}
+export default Navbar;
