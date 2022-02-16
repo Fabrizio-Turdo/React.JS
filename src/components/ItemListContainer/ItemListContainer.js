fa164f97a34bbe7837f4ee7185de3034ee09e895
Fabrizio-Turdo <fabrizioturdo1999@gmail.com>
2022-02-16 16:48:37 -0300

    arrayProductos

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -2,20 +2,32 @@ import Elementos from "./itemList";
 import img from "./Images/1.jpg"
 import img2 from "./Images/2.jpg"
 
+const data = [
+    {
+        img: {img},
+        title: "Elemento1",
+        description: "Remera",
+        detalles: "Mas info",
+    },
+    {
+        img: {img2}, //problema con la ruta de las IMG 
+        title: "Elemento2",
+        description: "Buzo",
+        detalles: "Mas info",
+    }
+];
 const ItemListContainer =  () => {
 
     return(
-        <>
+        <> 
+            {
+                data.map(item =>
+                    <Elementos img={item.img} title={item.title} 
+                    description={item.description} 
+                    detalles={item.detalles}/>
+                )
+            }
 
-            <div className="Elementos">
-                <Elementos img={img} title="Elemento1" 
-                description="remera estilo degrade" 
-                Detalles="Mas info"/>
-                <hr/>
-                <Elementos img={img2} title="Elemento2" 
-                description="buzo negro" 
-                Detalles="Mas info"/>
-            </div>
         </>
     );
 }
--- src/components/ItemListContainer/itemList.jsx
+++ src/components/ItemListContainer/itemList.jsx
@@ -1,13 +1,14 @@
 import './ItemList.css'
+
 const Elementos = (props) =>{
     return(
         <>
             <div className="card">
-            <img src={props.img} className="card-img-top" alt="..."></img>
+                <img src={props.img} className="card-img-top" alt="..."></img>
                 <div className="card-body">
                   <h5 className="card-title">{props.title}</h5>
                   <p className="card-text">{props.description}</p>
-                  <a href="#inico" className="btn btn-primary">{props.Detalles}</a>
+                  <a href="inico" className="btn btn-primary">{props.detalles}</a>
                 </div>
             </div>
         </>
