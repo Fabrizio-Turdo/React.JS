8d7701b9fc2b760f3713a44f3c5ed865744affe3
Fabrizio-Turdo <fabrizioturdo1999@gmail.com>
2022-03-01 20:41:43 -0300

    desafio-6,Terminado

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,4 +1,4 @@
-import Elementos from "./itemList";
+import Items from "./itemList";
 import ItemCount from "../ItemCount/ItemCount"
 import Data from "./Item"
 
@@ -12,12 +12,15 @@ const ItemListContainer =  (props) => {
         <> 
             <h1>{props.greeting}</h1>
             {
-                Elementos.map(item =>
-                    <Data 
+                Data.map(item =>
+                    <Items 
                     key={item.id}
-                    img={item.img} title={item.title} 
+                    img={item.img} 
+                    title={item.title} 
                     description={item.description} 
-                    detalles={item.detalles}/>
+                    detalles={item.detalles}
+                    stock={item.stock}/>
+                    
                 )
             }
             <ItemCount stock={5} initial={1} onAdd={onAdd} /> {/* Funcion onAdd. Agregar al carrito */}
--- src/components/ItemListContainer/itemList.jsx
+++ src/components/ItemListContainer/itemList.jsx
@@ -1,26 +1,19 @@
-const Data = [
-    {
-        id:"1",
-        img: "./Images/1.jpg",
-        title: "Elemento1",
-        description: "Remera",
-        detalles: "Mas info",
-    },
-    {
-        id:"2",
-        img:  "./Images/2.jpg", 
-        title: "Elemento2",
-        description: "Buzo",
-        detalles: "Mas info",
-    },
-    {
-        id:"3",
-        img:  "./Images/Pantalon-3.jpg", 
-        title: "Elemento3",
-        description: "Jean Satinado",
-        detalles: "Mas info",
-    }
+import './ItemList.css'
 
+const Items = (props) =>{
+    return(
+        <>
+            <div className="card">
+                <img src={props.img} className="card-img-top" alt="..."></img>
+                <div className="card-body">
+                  <h5 className="card-title">{props.title}</h5>
+                  <p className="card-text">{props.description}</p>
+                  <p className="card-text">{props.stock}</p>
+                  <a href="inico" className="btn btn-primary">{props.detalles}</a>
+                </div>
+            </div>
+        </>
+    )
+}
+export default Items;
 
-];
-export default Data;
